35cb66c5829c4889fa9ba2e4d6654872976e8da8
judelover27 <judelover27@gmail.com>
2025-02-06 17:48:28 +0900

    [feat] 몬스터 베이스

--- HellChangSub/HellChangSub/Monster.cs
+++ HellChangSub/HellChangSub/Monster.cs
@@ -8,5 +8,15 @@ namespace HellChangSub
 {
     class Monster
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Hp {  get; set; }
+        public int Att { get; set; }
+        public int Def { get; set; }
+        public int crit { get; set; }
+
+
+        public Monster() { }
+
     }
 }
