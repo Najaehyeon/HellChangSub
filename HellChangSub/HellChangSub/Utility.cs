94e1f896603c6a97a0b5cb4b35a3423dfdc638ea
judelover27 <judelover27@gmail.com>
2025-02-06 17:34:33 +0900

    [feature] 클래스 베이스 작업

---
+++ HellChangSub/HellChangSub/Utility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HellChangSub
+{
+    class Utility
+    {
+        public static int Select(int min, int max)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out int choice) && choice >=min && choice <=max)
+                {
+                    return choice;
+                }
+                else
+                {
+                    Console.WriteLine("잘못된 입력입니다.");
+                }
+            }
+        }
+    }
+
+
+}
