03ec92f63d0fb1f7419d99540f92b76b3394ea35
YooJoonYoung <ypin1003@naver.com>
2025-02-06 19:37:03 +0900

    Update Program.cs

--- HellChangSub/HellChangSub/Program.cs
+++ HellChangSub/HellChangSub/Program.cs
@@ -3,17 +3,13 @@
     internal class Program
     {
 
-        //sssssssssssss
-
-
-
-
-
-
+   
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
+            Console.WriteLine("원하시는 이름을 설정해주세요");
+            string input = Console.ReadLine();
         }
     }
 }
