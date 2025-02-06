75dd2eee4e3a432a1edcc27c6879c42c638789db
judelover27 <judelover27@gmail.com>
2025-02-06 20:21:44 +0900

    [fix] 유틸리티 롤백

--- HellChangSub/HellChangSub/Utility.cs
+++ HellChangSub/HellChangSub/Utility.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,29 +8,22 @@ namespace HellChangSub
 {
     class Utility
     {
-        public static int Select(string title, string[] options, string description)
+        public static int Select(int min, int max)
         {
-            Console.WriteLine($"\n{title}\n"); // 선택 제목 출력
-
-            for (int i = 0; i < options.Length; i++)
-            {
-                Console.WriteLine($"{i+1}. {options[i]}");
-            }
-
-            Console.WriteLine($"\n{description}");
-
             while (true)
             {
-                Console.Write(">> ");
-                if (int.TryParse(Console.ReadLine(), out int choice) && choice > 0 && choice <= options.Length)
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out int choice) && choice >=min && choice <=max)
                 {
                     return choice;
                 }
                 else
                 {
-                    Console.WriteLine("❌ 올바른 번호를 입력하세요.");
+                    Console.WriteLine("잘못된 입력입니다.");
                 }
             }
         }
     }
+
+
 }
