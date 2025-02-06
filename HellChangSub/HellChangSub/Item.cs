ad44ce1cd9eb05e557f137f2ac4a9ba1cd7c8c69
ssolem <tjrals961@gmail.com>
2025-02-06 19:21:16 +0900

    [feat] Item Class 정의
    Item Class 정의

---
+++ HellChangSub/HellChangSub/Item.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HellChangSub
+{
+    public class Item
+    {
+        public string Name { get; }
+        public string Description { get; }
+
+        public int Value { get; }
+        public int Price { get; }
+        public bool isPurchase { get; set; }
+        public bool isEquipt {  get; set; }
+
+        public Item(string name, int value, string description, int price)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            Value = value;
+            isPurchase = false;
+            isEquipt = false;
+        }
+       
+
+    }
+
+}
