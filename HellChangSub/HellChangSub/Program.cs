6793a0a78b2aaca81366f8589752a5307643b0f8
Najaehyeon <jaehyeon4832@gmail.com>
2025-02-06 17:53:48 +0900

    Update Program.cs

--- HellChangSub/HellChangSub/Program.cs
+++ HellChangSub/HellChangSub/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("스파르타 던전에 오신 걸 환영합니다.\n원하시는 이름을 설정해주세요.");
+            string name = Console.ReadLine();
+
         }
     }
 }
