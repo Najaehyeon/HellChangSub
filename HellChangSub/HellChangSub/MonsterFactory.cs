9a68f7ae9a8a42ebf4c734d5311924797b2c6517
limewing <58874904+limewing@users.noreply.github.com>
2025-02-07 09:42:52 +0900

    09:42

--- HellChangSub/HellChangSub/MonsterFactory.cs
+++ HellChangSub/HellChangSub/MonsterFactory.cs
@@ -8,45 +8,6 @@ namespace HellChangSub
 {
     class MonsterFactory
     {
-        private static Random rand = new Random();
-        public static Monster CreatMonster(int stageLvl)
-        {
-            int randomDice = rand.Next(0, 101);
-            int randomMonster;
-            if (randomDice < stageLvl * 5)
-            {
-                randomMonster = 3;
-            }
-            else if (randomDice < stageLvl * 10)
-            {
-                randomMonster = 2;
-            }
-            else if (randomDice < stageLvl * 20)
-            {
-                randomMonster = 1;
-            }
-            else
-            {
-                randomMonster = 0;
-            }
-            switch (randomMonster)
-            {
-                case 0:
-                    return new Slime(stageLvl);
-                    
-                case 1:
-
-                    break;
-                case 2:
-
-                    break;
-                case 3:
-
-                    break;
-
-                default:
-                    break;
-
-            }
-        }
+        
+    }
 }
