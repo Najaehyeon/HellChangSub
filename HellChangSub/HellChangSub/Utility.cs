a9a896506d2717bb45ac2b412fed28a29575b017
limewing <58874904+limewing@users.noreply.github.com>
2025-02-07 09:45:56 +0900

    09:45

--- HellChangSub/HellChangSub/Utility.cs
+++ HellChangSub/HellChangSub/Utility.cs
@@ -9,29 +9,6 @@ namespace HellChangSub
 {
     class Utility
     {
-        public static int Select(string title, string[] options, string description)
-        {
-            Console.WriteLine($"\n{title}\n"); // 선택 제목 출력
-
-            for (int i = 0; i < options.Length; i++)
-            {
-                Console.WriteLine($"{i+1}. {options[i]}");
-            }
-
-            Console.WriteLine($"\n{description}");
-
-            while (true)
-            {
-                Console.Write(">> ");
-                if (int.TryParse(Console.ReadLine(), out int choice) && choice > 0 && choice <= options.Length)
-                {
-                    return choice;
-                }
-                else
-                {
-                    Console.WriteLine("❌ 올바른 번호를 입력하세요.");
-                }
-            }
-        }
+        
     }
 }
