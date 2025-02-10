2b000ca0bf97ae8f80450eb0488aa70b13850f0c
judelover27 <judelover27@gmail.com>
2025-02-10 20:06:51 +0900

    [feat] 몬스터 스킬

--- HellChangSub/HellChangSub/Monster.cs
+++ HellChangSub/HellChangSub/Monster.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace HellChangSub
 {
-    interface Monster
+    class Monster
     {
         public string Name { get; set; }
         public string Description { get; set; }
@@ -21,7 +21,7 @@ namespace HellChangSub
         public bool IsDead => CurrentHealth <= 0;
         public int RewardExp { get; set; } //몬스터별 기본 보상 설정 필요
         public int RewardGold { get; set; } 
-
+        List<Skill> Skills = new List<Skill>();
 
     }
 
