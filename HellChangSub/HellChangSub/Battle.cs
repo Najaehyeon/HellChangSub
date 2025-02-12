e3b8b1c426138f45f0fedba8dc66a6314df6501b
limewing <58874904+limewing@users.noreply.github.com>
2025-02-12 14:25:46 +0900

    Update Battle.cs

--- HellChangSub/HellChangSub/Battle.cs
+++ HellChangSub/HellChangSub/Battle.cs
@@ -442,7 +442,7 @@ namespace HellChangSub
                 if (player.CurrentHealth < beforeHP)
                     Console.Write($"[데미지 : {damage}]{(Crit ? "" : " - 치명타 공격!!")}");
                 Console.WriteLine($"\nLv.{player.Level} {player.Name}");
-                Console.WriteLine(player.CurrentHealth < beforeHP? $"HP {beforeHP} -> {player.CurrentHealth}" : $"{player.Name}은(는) 공격을 완벽하게 막아내었다.\n");
+                Console.WriteLine(player.CurrentHealth < beforeHP? $"HP {beforeHP} -> {player.CurrentHealth}\n" : $"{player.Name}은(는) 공격을 완벽하게 막아내었다.\n");
             }
         }
 
